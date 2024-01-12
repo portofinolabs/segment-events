501df2f4f1c3f11cfeb4bb8c237d930ecd23e03a
Thomas Roberts <thomas@becausemarket.com>
2024-01-11 21:17:54 -0500

    setup js file

---
+++ checkout-events.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} apiKey - The API key for Segment tracking.
+ */
+(function (apiKey) {
+  /**
+   * Initializes Segment tracking for click events.
+   */
+  function initSegmentTracking() {}
+
+  initSegmentTracking();
+})();
