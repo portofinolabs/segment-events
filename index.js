fce4727c0225aa72a1cf83d87bc21488d777698e
Thomas Roberts <thomas@becausemarket.com>
2024-02-08 11:06:58 -0500

    update

--- index.js
+++ index.js
@@ -1,85 +1,131 @@
-import { AnalyticsBrowser, Context } from "@segment/analytics-next";
-import {
-  getCustomerInfo,
-  formatCheckoutData,
-  formatProductData,
-  formatEventName,
-} from "./utils.js";
+const UTM_SOURCE_QUERY = "utm_source";
+const UTM_MEDIUM_QUERY = "utm_medium";
+const UTM_CAMPAIGN_QUERY = "utm_campaign";
+const UTM_CONTENT_QUERY = "utm_content";
+const UTM_TERM_QUERY = "utm_term";
+const GCLID_QUERY = "gclid";
+const OID = "oid";
+const AFFID = "affid";
 
-const segment = (window.segment = AnalyticsBrowser.load({
-  writeKey: "bevOsoWqV9duPDtFxcCpLG7DJQbKdbLM",
-}));
+const UTM_QUERY_PARAMS = [
+  UTM_SOURCE_QUERY,
+  UTM_MEDIUM_QUERY,
+  UTM_CAMPAIGN_QUERY,
+  UTM_CONTENT_QUERY,
+  UTM_TERM_QUERY,
+  GCLID_QUERY,
+  OID,
+  AFFID,
+];
 
-console.log(segment);
-function initialize() {
-  const analytics = window.analytics;
-  const browser = window.browser;
 
-  analytics.subscribe("checkout_started", (event) => {
-    segment.track(
-      formatEventName(event.name),
-      formatCheckoutData(event.data.checkout)
-    );
-  });
 
-  analytics.subscribe("checkout_completed", (event) => {
-    const eventData = {
-      affiliation: "Website",
-      checkout_id: event.data.checkout.token,
-      coupon: event.data.checkout.discountApplications,
-      currency: event.data.checkout.currencyCode,
-      discount: event.data.checkout.discountApplications,
-      products: formatProductData(event.data.checkout.lineItems),
-      brand: event.data.checkout.lineItems[0].variant.product.vendor,
-      category: event.data.checkout.lineItems[0].variant.product.type,
-      image_url: event.data.checkout.lineItems[0].variant.image.src,
-      name: event.data.checkout.lineItems[0].variant.product.title,
-      price: event.data.checkout.lineItems[0].variant.price.amount,
-      product_id: event.data.checkout.lineItems[0].variant.product.id,
-      quantity: event.data.checkout.lineItems[0].quantity,
-      sku: event.data.checkout.lineItems[0].variant.sku,
-      subscription: !!event.data.checkout.lineItems[0].selling_plan_allocation,
-      url: `${window.location.hostname}${event.data.checkout.lineItems[0].variant.product.url}`,
-      value: (
-        event.data.checkout.lineItems[0].variant.price.amount *
-        event.data.checkout.lineItems[0].quantity
-      ).toFixed(2),
-      variant: event.data.checkout.lineItems[0].variant.title,
-      source: "becausemarket.com",
-    };
+function extractUTMParams() {
+  const query = new URLSearchParams(location.search);
+  const utmSource = query.get(UTM_SOURCE_QUERY);
+  console.log(utmSource);
 
-    segment.track(formatEventName(event.name), eventData);
-  });
+  if (!utmSource) {
+    return undefined;
+  }
 
-  analytics.subscribe("page_viewed", async (event) => {
-    let pageData;
-    const { navigator, document } = event;
+  return UTM_QUERY_PARAMS.reduce((acc, param) => {
+    const value = query.get(param);
 
-    if (document && navigator) {
-      pageData = {
-        title: document.title,
-        path: document.location.pathname,
-        referrer: document.referrer,
-        search: document.location.search,
-        url: document.location.href,
-        keywords: [],
-        userAgent: navigator.userAgent,
-        userAgentData: [],
-      };
+    if (!value) {
+      return acc;
     }
-    const eventData = {
-      ...(await getCustomerInfo()),
-      ...pageData,
-    };
 
-    segment.page(formatEventName(event.name), eventData);
-  });
-}
+    acc[param] = query.get(param);
+    return acc;
+  }, {});
+};
 
-if (document.readyState === "loading") {
-  // If document is still loading
-  document.addEventListener("DOMContentLoaded", initialize); // Wait for it to finish loading before running initialize
-} else {
-  // Otherwise, if document has already loaded
-  initialize(); // Run initialize immediately
-}
+function storeUTMParams() {
+  const utmParams = extractUTMParams();
+  console.log(utmParams);
+  if (!utmParams) {
+    return;
+  }
+  sessionStorage.setItem("utmParams", JSON.stringify(utmParams));
+};
+
+function getUTMParams() {
+  const sessionUtm = sessionStorage.getItem("utmParams");
+  console.debug("sessionUtm", sessionUtm);
+  const urlUtm = JSON.stringify(extractUTMParams());
+  const utmParams = sessionUtm || urlUtm;
+  console.debug("sessionUtm", sessionUtm);
+  console.debug("urlUtm", urlUtm);
+
+  if (!utmParams) {
+    return undefined;
+  }
+
+  return JSON.parse(utmParams);
+};
+
+function getUTMParamsAsStringArray() {
+  const utmParams = getUTMParams();
+
+  if (!utmParams) {
+    return undefined;
+  }
+
+  return Object.entries(utmParams).reduce((acc, [key, value]) => {
+    acc.push(`${key}=${value}`);
+    return acc;
+  }, []);
+};
+
+function removeUMTParams() {
+  const utmParams = extractUTMParams();
+  const url = new URL(global.location.href);
+  if (utmParams) {
+    for (let param in utmParams) {
+      if (Object.hasOwnProperty.call(utmParams, param)) {
+        url.searchParams.delete(param);
+      }
+    }
+
+    global.history.pushState({}, "", url.toString());
+  }
+};
+
+
+function getCustomerPortalInfo() {
+  // Retrieve the URL from sessionStorage
+  let storedUrl = JSON.parse(sessionStorage.getItem('_attn_'));
+  let customerId = "";
+  let subscriptionId = "";
+
+  // Check if the URL is not null or undefined
+  if (storedUrl && storedUrl['pd']) {
+    var urlParts = storedUrl['pd'].split('/');
+
+    // Find the index of 'customer' and 'subscription'
+    var customerIndex = urlParts.indexOf('customer');
+    var subscriptionIndex = urlParts.indexOf('subscription');
+
+    // If 'customer' exists
+    if (customerIndex !== -1 && urlParts.length > customerIndex + 1) {
+      customerId = urlParts[customerIndex + 1].split('?')[0]; // Get customer ID
+      console.log("Customer ID: ", customerId);
+    } else {
+      console.log("Customer ID not found in the URL.");
+    }
+
+    // If 'subscription' exists
+    if (subscriptionIndex !== -1 && urlParts.length > subscriptionIndex + 1) {
+      subscriptionId = urlParts[subscriptionIndex + 1].split('?')[0]; // Get subscription ID
+      console.log("Subscription ID: ", subscriptionId);
+    } else {
+      console.log("Subscription ID not found in the URL.");
+    }
+  } else {
+    console.log("URL not found in sessionStorage.");
+  }
+
+  return { customerId, subscriptionId }
+};
+
