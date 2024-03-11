061f91d1b4d9f8ecba934b260df81b997e837c88
Thomas Roberts <thomas@becausemarket.com>
2024-03-11 13:19:24 -0400

    move snippet to theme

--- index.js
+++ index.js
@@ -6,19 +6,23 @@
  */
 
 
-const writeKey = window.location.host.includes('qa2') || !!window.location.port ? "bevOsoWqV9duPDtFxcCpLG7DJQbKdbLM" : "41NA4iurxaSbBVPPhBhZ1HVygV7NvUzm";
-!function () {
-  var i = "segment", analytics = window[i] = window[i] || []; if (!analytics.initialize) if (analytics.invoked) window.console && console.error && console.error("Segment snippet included twice."); else {
-    analytics.invoked = !0; analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "reset", "group", "track", "ready", "alias", "debug", "page", "screen", "once", "off", "on", "addSourceMiddleware", "addIntegrationMiddleware", "setAnonymousId", "addDestinationMiddleware", "register"]; analytics.factory = function (e) { return function () { if (window[i].initialized) return window[i][e].apply(window[i], arguments); var n = Array.prototype.slice.call(arguments); if (["track", "screen", "alias", "group", "page", "identify"].indexOf(e) > -1) { var c = document.querySelector("link[rel='canonical']"); n.push({ __t: "bpc", c: c && c.getAttribute("href") || void 0, p: location.pathname, u: location.href, s: location.search, t: document.title, r: document.referrer }) } n.unshift(e); analytics.push(n); return analytics } }; for (var n = 0; n < analytics.methods.length; n++) { var key = analytics.methods[n]; analytics[key] = analytics.factory(key) } analytics.load = function (key, n) { var t = document.createElement("script"); t.type = "text/javascript"; t.async = !0; t.setAttribute("data-global-segment-analytics-key", i); t.src = "https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js"; var r = document.getElementsByTagName("script")[0]; r.parentNode.insertBefore(t, r); analytics._loadOptions = n }; analytics._writeKey = "bevOsoWqV9duPDtFxcCpLG7DJQbKdbLM";; analytics.SNIPPET_VERSION = "5.2.0";
-    analytics.load(writeKey);
-  }
-}();
+// const writeKey = window.location.host.includes('qa2') || !!window.location.port ? "bevOsoWqV9duPDtFxcCpLG7DJQbKdbLM" : "41NA4iurxaSbBVPPhBhZ1HVygV7NvUzm";
+// !function () {
+//   var i = "segment", analytics = window[i] = window[i] || []; if (!analytics.initialize) if (analytics.invoked) window.console && console.error && console.error("Segment snippet included twice."); else {
+//     analytics.invoked = !0; analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "reset", "group", "track", "ready", "alias", "debug", "page", "screen", "once", "off", "on", "addSourceMiddleware", "addIntegrationMiddleware", "setAnonymousId", "addDestinationMiddleware", "register"]; analytics.factory = function (e) { return function () { if (window[i].initialized) return window[i][e].apply(window[i], arguments); var n = Array.prototype.slice.call(arguments); if (["track", "screen", "alias", "group", "page", "identify"].indexOf(e) > -1) { var c = document.querySelector("link[rel='canonical']"); n.push({ __t: "bpc", c: c && c.getAttribute("href") || void 0, p: location.pathname, u: location.href, s: location.search, t: document.title, r: document.referrer }) } n.unshift(e); analytics.push(n); return analytics } }; for (var n = 0; n < analytics.methods.length; n++) { var key = analytics.methods[n]; analytics[key] = analytics.factory(key) } analytics.load = function (key, n) { var t = document.createElement("script"); t.type = "text/javascript"; t.async = !0; t.setAttribute("data-global-segment-analytics-key", i); t.src = "https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js"; var r = document.getElementsByTagName("script")[0]; r.parentNode.insertBefore(t, r); analytics._loadOptions = n }; analytics._writeKey = "bevOsoWqV9duPDtFxcCpLG7DJQbKdbLM";; analytics.SNIPPET_VERSION = "5.2.0";
+//     analytics.load('bevOsoWqV9duPDtFxcCpLG7DJQbKdbLM');
+//   }
+// }();
 
 const analytics = window.shopevents
+console.log('analytics', window.analytics)
+console.log('segment', window.segment)
+console.log('shopevents', analytics)
 
 const getSegment = async () => {
   return await window.analytics
 }
+
 
 
 
@@ -250,7 +254,7 @@ const productAddedEvent = async (data) => {
       wishlist_id: null,
       wishlist_name: null,
       user_id: customerId || window?.analytics?.user().id(),
-      anonymous_id: window.window?.analytics?.user().anonymousId(),
+      anonymous_id: window?.analytics?.user().anonymousId(),
     };
 
     segment.identify()
@@ -283,7 +287,7 @@ const productRemovedEvent = async (product) => {
       url: window.location.href,
       value: formatProductPrice(product.final_line_price),
       user_id: customerId || window?.analytics?.user().id(),
-      anonymous_id: window.window?.analytics?.user().anonymousId(),
+      anonymous_id: window?.analytics?.user().anonymousId(),
     };
 
     window?.analytics?.track("Product Removed From Cart", eventData, {
